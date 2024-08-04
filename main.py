7be7a750783b01f8360cb8cac95b25a0f229e2ff
Wuwill <abc1276@kimo.com>
2024-08-04 14:43:47 +0800

    初始化

---
+++ main.py
@@ -0,0 +1,21 @@
+"""這個.py是在做路由轉換的"""
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__) # __name__ 代表目前執行的模組
+
+@app.route('/')
+def index():
+    """視圖函式 view function"""
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/register')
+def register():
+    
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    
+    return render_template('login.html')
