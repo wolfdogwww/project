cdf0a4464af9233329c22303413ba97cf9a017c7
Wuwill <abc1276@kimo.com>
2024-08-12 17:53:33 +0800

    add logsetup and model_user function library

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """這個.py是在做路由轉換的"""
 
 from flask import Flask, render_template, request, jsonify
-
+import logging
+from method.logsetup import logsetup
+from method.model_user import create_user
 app = Flask(__name__) # __name__ 代表目前執行的模組
 
 @app.route('/')
@@ -23,3 +25,5 @@ def login():
 if __name__ == '__main__':
     app.debug = True
     app.run()
+    create_user()
+    logsetup()
